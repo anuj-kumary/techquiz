d5488f1849bf27f68a6e46e310dc20b48709d398
anuj-kumary <anujf0510@gmail.com>
2022-04-26 21:46:19 +0530

    feat : Home Page

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+export const Home=() =>{
+  return (
+    <div className='hero'>
+        <h1 className='heading'>techQuiz</h1>
+        <p className='sub__heading'>Could you be our lucky winner ?</p>
+        <button className='btn btn__primary'>Let's find out</button>
+    </div>
+  )
+}
